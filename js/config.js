45f2fb11b1466d10eb0d628ded1d454a999c713e
Yousif Al hamadani <75411145+yousifayad577@users.noreply.github.com>
2023-04-22 16:55:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
+        userPoolId:us-east-1_50EmdpRbD         '',         // e.g. us-east-2_uXboG5pAb
         userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             ''          // e.g. us-east-2
     },
