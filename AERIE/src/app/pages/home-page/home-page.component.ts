422e47e9be0b2004d450155a20b9f661a7bece0b
jared singleton <singletonja89@gmail.com>
2023-09-13 16:59:50 -0500

    navbar

--- AERIE/src/app/pages/home-page/home-page.component.ts
+++ AERIE/src/app/pages/home-page/home-page.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  onLogin: boolean = true;
 
+  toggleLogin(): void {
+    this.onLogin = !this.onLogin;
+    console.log("toggle login");
+  }
 }
