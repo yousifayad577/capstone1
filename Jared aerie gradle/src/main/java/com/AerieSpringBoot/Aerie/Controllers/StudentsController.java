c9a71525c4514aecc393eae9b7497520726c857f
Fatima Shibli <95264693+FatimaShibli@users.noreply.github.com>
2023-11-11 19:52:03 -0600

    Update StudentsController.java

--- Jared aerie gradle/src/main/java/com/AerieSpringBoot/Aerie/Controllers/StudentsController.java
+++ Jared aerie gradle/src/main/java/com/AerieSpringBoot/Aerie/Controllers/StudentsController.java
@@ -4,34 +4,114 @@
 package com.AerieSpringBoot.Aerie.Controllers;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.AerieSpringBoot.Aerie.Students;
+import com.AerieSpringBoot.Aerie.Auth.JwtResponse;
+import com.AerieSpringBoot.Aerie.Auth.JwtTokenProvider;
 import com.AerieSpringBoot.Aerie.Services.StudentService;
 
-@RestController
-@RequestMapping("/api/v1/students")
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1/Student")
 public class StudentsController {
+
     @Autowired
     private StudentService studentService;
 
-    @GetMapping()
-    public ResponseEntity<List<Students>> getAllStudents() {
-        return new ResponseEntity<List<Students>>(studentService.getAllStudents(), HttpStatus.OK);
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    // Constructor
+    public StudentsController(StudentService studentService) {
+        this.studentService = studentService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Students>> getSingleStudent(@PathVariable ObjectId id) {
-        return new ResponseEntity<Optional<Students>>(studentService.getSingleStudent(id), HttpStatus.OK);
+    // @GetMapping
+    // public List<Students> getStudents() {
+    // return studentService.getStudents();
+    // }
+    @GetMapping
+    public ResponseEntity<List<Students>> getStudents() {
+        return new ResponseEntity<List<Students>>(studentService.getStudents(), HttpStatus.OK);
     }
-    
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Students student) {
+        studentService.addNewStudent(student);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerStudent(@RequestBody Students student, UriComponentsBuilder uriComponentsBuilder) {
+        Students registeredStudent = studentService.addNewStudent(student);
+        // You can generate a JWT token here and return it in the response.
+        String token = jwtTokenProvider.createToken(registeredStudent.getEmail());
+
+        return ResponseEntity
+                .created(uriComponentsBuilder.path("/api/users/{email}")
+                        .buildAndExpand(registeredStudent.getEmail()).toUri())
+                .body(new JwtResponse(token));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginStudent(@RequestBody Students student) {
+        String email = student.getEmail();
+        String password = student.getPassword();
+
+        // Validate user credentials here, and if they are valid, generate a JWT token.
+        if (studentService.isValidUser(email, password)) {
+            String token = jwtTokenProvider.createToken(email);
+            return ResponseEntity.ok(new JwtResponse(token));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+    }
+
+    // Find student credentials by email for login
+    // @PostMapping(value = "/login")
+    // // public ResponseEntity<AuthenticationResponse> loginStudent(@RequestBody
+    // AuthenticationRequest request) {
+    // public ResponseEntity<Students> loginStudent(@RequestBody Map<String, String>
+    // loginData) {
+    // // String email = loginData.get("email");
+    // // String password = loginData.get("password");
+
+    // Students student = studentService.getStudent(loginData.get("email"));
+
+    // if(student != null && (student.getPassword() == loginData.get("password"))) {
+    // return new ResponseEntity<Students>(student, HttpStatus.OK);
+    // }
+
+    // // return ResponseEntity.ok(studentService.authenticate(request));
+    // return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    // }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") String studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") String studentId,
+            @RequestParam(required = false) String first_name,
+            @RequestParam(required = false) String last_name,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String major,
+            @RequestParam(required = false) String password) {
+
+        studentService.updateStudent(studentId, first_name, last_name, email, major, password);
+    }
 }
