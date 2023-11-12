70d050117419d9edcde38504b6ce544bf514cc7a
Fatima Shibli <95264693+FatimaShibli@users.noreply.github.com>
2023-11-11 19:50:25 -0600

    Add files via upload

---
+++ Jared aerie gradle/src/main/java/com/AerieSpringBoot/Aerie/Auth/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.AerieSpringBoot.Aerie.Auth;
+
+public class JwtResponse {
+    private final String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
